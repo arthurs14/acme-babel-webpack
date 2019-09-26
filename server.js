1663aa6796d4cea6586a6cf2f754a32cb9e84322
Arthur <arthurcs426@gmail.com>
2019-09-25 20:29:18 -0700

    got express working

--- server.js
+++ server.js
@@ -11,5 +11,26 @@ app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/api/people', (req, res, next) => {
+  res.send([
+    { id: 1, name: 'Nathan' },
+    { id: 2, name: 'Arthur' }
+  ]);
+});
+
+app.get('/api/places', (req, res, next) => {
+  res.send([
+    { id: 1, name: 'Paris' },
+    { id: 2, name: 'Berlin'}
+  ])
+});
+
+app.get('/api/things', (req, res, next) => {
+  res.send([
+    { id: 1, name: 'Hammer' },
+    { id: 2, name: 'Wrench' }
+  ]);
+});
+
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
